fe0a030233bd2d7e0c40d3a36f102c6a84c4b62b
Matthew Weis <matthewweis@ksu.edu>
2022-01-30 04:15:45 -0600

    readme

---
+++ tck/src/test/java/module-info.java
@@ -0,0 +1,5 @@
+module _GROUP_TLD_._GROUP_DOMAIN_NAME_._ARTIFACT_ID_.impl.test {
+    requires  _GROUP_TLD_._GROUP_DOMAIN_NAME_._ARTIFACT_ID_.impl;
+    exports   _GROUP_TLD_._GROUP_DOMAIN_NAME_._ARTIFACT_ID_.impl.integration;
+    exports   _GROUP_TLD_._GROUP_DOMAIN_NAME_._ARTIFACT_ID_.impl.unit;
+}
